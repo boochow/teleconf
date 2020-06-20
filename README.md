5c7e101f03969809e333bde87540524c55b52467
boochow <boochowp@gmail.com>
2020-06-20 13:56:37 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Teleconf
+
+## About
+
+Teleconf is a modulation unit for Korg loge SDK compatible synthesizers.
+
+It modulates input sounds as if it comes from a teleconference device.
+
+Technically this module applies a bandpass filter and [G.711 a-law](https://en.wikipedia.org/wiki/G.711) codec to input sounds.
---
+++ teleconf.cpp
@@ -0,0 +1,125 @@
+/*
+ * File: teleconf.cpp
+ */
+
+#include "usermodfx.h"
+#include "biquad.hpp"
+
+// filter parameters 
+#define LPF_FC 1400.f
+#define HPF_FC 300.f
+#define LPF_Q  0.5f
+
+// filters before downsampling
+static dsp::BiQuad s_bq_lpf;
+static dsp::BiQuad s_bqs_lpf;
+static dsp::BiQuad s_bq_hpf;
+static dsp::BiQuad s_bqs_hpf;
+// filters after downsampling
+static dsp::BiQuad s_bq_lpf_out;
+static dsp::BiQuad s_bqs_lpf_out;
+static dsp::BiQuad s_bq_lpf_out2;
+static dsp::BiQuad s_bqs_lpf_out2;
+
+static const float s_fs_recip = 1.f / 48000.f;
+
+static float amp = 15.f;
+static float dry = 0.f;
+
+static uint32_t count = 0;
+static float lastmy;
+static float lastsy;
+
+void init_lpf(const float f, const float q) {
+    float wc = s_bq_lpf.mCoeffs.wc(f, s_fs_recip);
+    s_bq_lpf.flush();
+    s_bqs_lpf.flush();
+    s_bq_lpf_out.flush();
+    s_bqs_lpf_out.flush();
+    s_bq_lpf_out2.flush();
+    s_bqs_lpf_out2.flush();
+    s_bq_lpf.mCoeffs.setSOLP(fx_tanpif(wc), q);
+    s_bqs_lpf.mCoeffs     = s_bq_lpf.mCoeffs;
+    s_bq_lpf_out.mCoeffs  = s_bq_lpf.mCoeffs;
+    s_bqs_lpf_out.mCoeffs = s_bq_lpf.mCoeffs;
+    s_bq_lpf_out2.mCoeffs  = s_bq_lpf.mCoeffs;
+    s_bqs_lpf_out2.mCoeffs = s_bq_lpf.mCoeffs;
+}
+
+void MODFX_INIT(uint32_t platform, uint32_t api)
+{
+    init_lpf(LPF_FC, LPF_Q);
+    float wc = s_bq_hpf.mCoeffs.wc(HPF_FC, s_fs_recip);
+    s_bq_hpf.flush();
+    s_bqs_hpf.flush();
+    s_bq_hpf.mCoeffs.setFOHP(fx_tanpif(wc));
+    s_bqs_hpf.mCoeffs = s_bq_hpf.mCoeffs;
+}
+
+__fast_inline float g711(const float s) {
+    q15_t val;
+    int16_t sign =1;
+    uint16_t mask = 1 << 14;
+    int i;
+    val = f32_to_q15(s);
+    sign = (val < 0) ? -1 : 1;
+    val = q15abs(val);
+    for(i = 0; i < 6; i++) {
+        if (val & mask)
+            mask >>= 1;
+        else
+            break;
+    }
+    mask = 0b1111000 << (7 - i);
+    val = sign * (val & mask);
+    return q15_to_f32(val);
+}
+
+void MODFX_PROCESS(const float *main_xn, float *main_yn,
+                   const float *sub_xn,  float *sub_yn,
+                   uint32_t frames)
+{
+  const float * mx = main_xn;
+  float * __restrict my = main_yn;
+  const float * my_e = my + 2*frames;
+
+  const float * sx = sub_xn;
+  float * __restrict sy = sub_yn;
+  
+  float vmx;
+  float vsx;
+  float dmy;
+  float dsy;
+  for (; my != my_e; ) {
+      vmx = s_bq_hpf.process_fo(s_bq_lpf.process_so(*mx++));
+      vsx = s_bqs_hpf.process_fo(s_bqs_lpf.process_so(*sx++));
+
+      if (count == 0) {
+          lastmy = g711(vmx);
+          lastsy = g711(vsx);
+      }
+      count = (count + 1) % 6;
+
+      *my++ = dry * vmx + (1 - dry) * \
+          s_bq_lpf_out2.process_so(s_bq_lpf_out.process_so(lastmy));
+      *sy++ = dry * vsx + (1 - dry) * \
+          s_bq_lpf_out2.process_so(s_bqs_lpf_out.process_so(lastsy));
+  }
+}
+
+void MODFX_PARAM(uint8_t index, int32_t value)
+{
+  const float valf = q31_to_f32(value);
+
+  switch (index) {
+  case k_user_modfx_param_time:
+      init_lpf(LPF_FC, LPF_Q  + 1.6 * valf);
+      amp = 20 - valf * 5;
+    break;
+  case k_user_modfx_param_depth:
+      dry = valf;
+    break;
+  default:
+    break;
+  }
+}
